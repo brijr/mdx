6b162e7d98b0f40571a06e431439747dff3e71bb
Bridger Tower <bridgertower@gmail.com>
2025-11-10 11:35:32 -0700

    refactor component structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { Main, Section, Container, Prose } from "@/components/ds";
 import { PostsList } from "@/components/posts/posts-list";
-import { Logo } from "@/components/logo";
+import { Hero } from "@/components/site/hero";
+import { Main } from "@/components/ds";
 
 import { getAllPosts } from "@/lib/posts";
 
