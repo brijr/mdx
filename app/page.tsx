997265c86ebff1e6948738971881af988ccc2e9a
Bridger Tower <bridgertower@gmail.com>
2025-11-09 23:03:56 -0700

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ const Posts = ({ posts }: { posts: Post[] }) => {
       <Container className="space-y-6">
         <h2 className="text-2xl font-semibold tracking-tight">Recent Posts</h2>
         {posts.length > 0 ? (
-          <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid gap-4">
             {posts.map((post) => (
               <PostCard
                 key={post.slug}
